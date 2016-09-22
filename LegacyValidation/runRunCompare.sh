da98a2c9e927c1b5c2ee984d7925b096958dd726
Gabriel Perdue <perdue@fnal.gov>
2016-09-22 12:24:09 -0500

    point to more recent trunk build

--- LegacyValidation/runRunCompare.sh
+++ LegacyValidation/runRunCompare.sh
@@ -2,8 +2,15 @@
 
 BUILDS=/grid/fermiapp/genie/builds/
 COMPPATH=`pwd`
+
 ./compare.py --builds $BUILDS \
     --genie_tags 'R-2_10_10 trunk' \
-    --genie_dates '2016-07-29 2016-09-12' \
+    --genie_dates '2016-07-29 2016-09-15' \
     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
     --comp_path ${COMPPATH}/runCompare.sh
+
+# ./compare.py --builds $BUILDS \
+#     --genie_tags 'trunk R-2_10_10' \
+#     --genie_dates '2016-09-15 2016-07-29' \
+#     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
+#     --comp_path ${COMPPATH}/runCompare.sh
