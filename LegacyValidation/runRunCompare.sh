38cbb30565ad2345dfad472df0d5b09aa0422b95
Gabriel Perdue <perdue@fnal.gov>
2016-10-11 16:07:35 -0500

    misc updates - nothing important

--- LegacyValidation/runRunCompare.sh
+++ LegacyValidation/runRunCompare.sh
@@ -5,12 +5,12 @@ COMPPATH=`pwd`
 
 ./compare.py --builds $BUILDS \
     --genie_tags 'R-2_10_10 trunk' \
-    --genie_dates '2016-07-29 2016-09-15' \
+    --genie_dates '2016-09-22 2016-10-05' \
     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
     --comp_path ${COMPPATH}/runCompare.sh
 
 # ./compare.py --builds $BUILDS \
-#     --genie_tags 'trunk R-2_10_10' \
-#     --genie_dates '2016-09-15 2016-07-29' \
+#     --genie_tags 'trunk R-2_10_2' \
+#     --genie_dates '2016-09-23 2016-02-22' \
 #     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
 #     --comp_path ${COMPPATH}/runCompare.sh
