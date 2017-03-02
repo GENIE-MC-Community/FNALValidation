5c65a5bb451c390dc667ec55d517e2f90417b6f5
Gabriel Perdue <perdue@fnal.gov>
2017-03-02 17:24:16 -0600

    run 2.12.4

--- LegacyValidation/runRunVal.sh
+++ LegacyValidation/runRunVal.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 TAG=trunk
-TAG=R-2_12_2
+TAG=R-2_12_4
+
+DEBUG=""
+DEBUG="--debug true"
+
 VALPATH=/grid/fermiapp/genie/validation/FNALValidation/LegacyValidation
 BUILDS=/grid/fermiapp/genie/builds
 OUTDIR=/pnfs/genie/scratch/users/$USER/legacyValidation
-DEBUG="--debug true"
+
 
 python runLegacyValidation.py --genie_tag $TAG $DEBUG \
     --run_path $VALPATH/runGENIE.sh \
