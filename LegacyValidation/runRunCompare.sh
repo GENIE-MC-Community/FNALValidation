3578135ec7411361415cccc05226334373c63b62
Gabriel Perdue <perdue@fnal.gov>
2017-06-19 08:33:54 -0500

    compare 2.12.4 and 2.12.6

--- LegacyValidation/runRunCompare.sh
+++ LegacyValidation/runRunCompare.sh
@@ -4,10 +4,16 @@ BUILDS=/grid/fermiapp/genie/builds/
 COMPPATH=`pwd`
 
 ./compare.py --builds $BUILDS \
-    --genie_tags 'trunk trunk' \
-    --genie_dates '2017-03-07 2017-03-20' \
+    --genie_tags 'R-2_12_4 R-2_12_6' \
+    --genie_dates '2017-06-16 2017-06-07' \
     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
     --comp_path ${COMPPATH}/runCompare.sh
+
+# ./compare.py --builds $BUILDS \
+#     --genie_tags 'trunk trunk' \
+#     --genie_dates '2017-03-07 2017-03-20' \
+#     --top_dir /pnfs/genie/scratch/users/perdue/legacyValidation/ \
+#     --comp_path ${COMPPATH}/runCompare.sh
 
 # ./compare.py --builds $BUILDS \
 #     --genie_tags 'R-2_12_4 R-2_12_2' \
