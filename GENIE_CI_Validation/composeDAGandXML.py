5b561a0f310ab08c4f1fa13cd287330c1486d035
Julia Yarba <yarba_j@fnal.gov>
2018-01-30 16:31:17 -0600

    start cutting a genie_ci-oriented version from LegacyValidation

---
+++ GENIE_CI_Validation/composeDAGandXML.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from jobsub import Jobsub
+# services
+import parser, msg
+import outputPaths
+# xsec splines
+import nun, nua
+# new-style validation (minerva, etc.)
+import minerva
+# general
+import os, datetime
+
+# example format:
+# ./runLegacyValidation.py --genie_tag trunk  \ 
+#                          --build_date YYYY-MM-DD  \
+#                          --run_path /path/to/runGENIE.sh \ # e.g. /grid/fermiapp/genie/legacyValidation_update_1/runGENIE.sh
+#                          --builds DUMMY \ 
+#                          --output OUTPUT \ # e.g. /pnfs/genie/scratch/users/yarba_j/GENIE_LegacyValidation 
+#  optional:               [ --regre 'R-2_12_6/2017-09-11 [reg2, reg3,...]' --regre_dir /pnfs/genie/persistent/users/yarba_j/GENIE_LegacyValidation ]
+
+def initMessage (args):
+  print msg.BLUE
+  print '*' * 80
+  print '*', ' ' * 76, '*'
+  print "*\tGENIE Legacy Validation based on src/scripts/production/batch", ' ' * 8, '*'
+  print '*', ' ' * 76, '*'
+  print '*' * 80
+  print msg.GREEN
+  print "Configuration:\n"
+  print "\tGENIE version:\t", args.tag
+  print "\tBuild on:\t", args.build_date
+  print "\tLocated at:\t", args.builds
+  print "\n\tResults folder:", args.output
+  print msg.END
+
+if __name__ == "__main__":
+  
+  
+  # parse command line arguments
+  args = parser.getArgs()
+  print "CHECK DATE: ", args.build_date
+  # if build date is not defined/specified, use today's date as default
+  if args.build_date is None:
+     print "DATE is None, reseting it to DEFAULT(today)"
+     #
+     # NOTE-1: os.system('date +%Y-%m-%d') will return an integer status !!!
+     # NOTE-2: os.popen('date +%Y-%m-%d').read() will results in the "?" question mark at the end of the string
+     # NOTE-3: the "%y-%m-%d" will result in the YY-MM-DD format
+     #
+     args.build_date = datetime.date.today().strftime("%Y-%m-%d") 
+  
+  # print configuration summary
+  initMessage (args)
+
+  # preapre folder structure for output
+  args.paths = outputPaths.prepare (args.output + "/" + args.tag + "/" + args.build_date)
+
+  # initialize jobsub
+  args.buildNameGE = "generator_" + args.tag + "_" + args.build_date
+  args.buildNameCmp = "comparisons_" + args.cmptag + "_" + args.build_date
+  jobsub = Jobsub (args)
+
+  # fill dag files with jobs
+  msg.info ("Adding jobs to dag file: " + jobsub.dagFile + "\n")
+  # nucleon cross sections
+  nun.fillDAG (jobsub, args.tag, args.paths)
+  # nucleus cross sections
+  nua.fillDAG (jobsub, args.tag, args.paths)
+  # MINERvA test
+  minerva.fillDAG( jobsub, args.tag, args.build_date, args.paths, args.regretags, args.regredir )
---
+++ GENIE_CI_Validation/outputPaths.py
@@ -0,0 +1,30 @@
+
+import os
+
+def prepare (path):
+  # create a dictionary for output paths
+  paths = {}
+  paths['top'] = path
+  # splines
+  paths['xsec']   = path + "/xsec"
+  paths['xsec_N'] = path + "/xsec/nuN"
+  paths['xsec_A'] = path + "/xsec/nuA"
+  # events
+  paths['events']  = path + "/events"
+  # paths['mctest']  = path + "/events/mctest"
+  # paths['reptest'] = path + "/events/repeatability"
+  # paths['xsecval'] = path + "/events/xsec_validation"
+  # paths['hadron']  = path + "/events/hadronization"
+  paths['minerva'] = path + "/events/minerva"
+  # reports
+  paths['reports'] = path + "/reports"
+  # paths['sanity']  = path + "/reports/sanity_mctest"
+  # paths['replog']  = path + "/reports/repeatability_test"
+  # paths['xseclog'] = path + "/reports/xsec_validation"
+  # paths['hadrep']  = path + "/reports/hadronization_test"
+  paths['minervarep'] = path + "/reports/minerva"
+  # create all directiories
+  for p in paths.values():
+    if not os.path.exists (p): os.makedirs (p)
+  # return paths dictionary
+  return paths
